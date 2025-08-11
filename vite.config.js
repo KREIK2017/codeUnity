9f336927ebdb78f03b38f0468fd2ae1492e6aeee
Мычко Максим <maksimkamychko@gmail.com>
2025-08-11 13:29:58 +0300

    change start for netify and gitpage

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,42 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
+// export default defineConfig({
+//     root: 'src',
+//     build: {
+//         outDir: '../dist',
+//         rollupOptions: {
+//             input: {
+//                 main: resolve(__dirname, 'src/test/index.html'),
+//                 // main: resolve(__dirname, 'src/day2/index.html'),
+
+//             },
+//         },
+//     },
+//     server: {
+//         port: 3000,
+//         open: '/test/index.html' // Вказуємо, яку сторінку відкривати
+//         // open: '/day2/index.html'
+
+//     },
+//     assetsInclude: ['**/*.glb']
+// });
+
 export default defineConfig({
+    // CHANGE THIS TO YOUR REPO NAME
+    base: '/codeUnity/',
     root: 'src',
     build: {
         outDir: '../dist',
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'src/test/index.html'),
-                // main: resolve(__dirname, 'src/day2/index.html'),
-
             },
         },
     },
     server: {
         port: 3000,
-        open: '/test/index.html' // Вказуємо, яку сторінку відкривати
-        // open: '/day2/index.html'
-
+        open: '/test/index.html'
     },
     assetsInclude: ['**/*.glb']
 });
