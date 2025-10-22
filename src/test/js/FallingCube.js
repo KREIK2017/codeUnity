64690dbf3cfc0bd373d6fbf9279e4e27fb34ae1e
KREIK2017 <maksimkamychko@gmail.com>
2025-10-22 12:30:22 +0300

    test shadows

--- src/test/js/FallingCube.js
+++ src/test/js/FallingCube.js
@@ -1,11 +1,11 @@
 import * as THREE from 'three';
 
-const INITIAL_Y_POSITION = 37;
+const INITIAL_Y_POSITION = 0;
 
 // Змінні для анімації падіння
 const fallDistance = 35.2; // Як далеко вниз впаде куб
 const fallStartScroll = 0.1; // Падіння починається після 20% прокрутки
-const fallEndScroll = 0.8;   // і закінчується на 80%
+const fallEndScroll = 0.8; // і закінчується на 80%
 
 // Зберігаємо початкову позицію куба
 const cubeStartPosition = new THREE.Vector3(0, 0.75, 0);
@@ -14,7 +14,7 @@ export function createFallingCubeScene() {
     const group = new THREE.Group();
     group.position.y = INITIAL_Y_POSITION;
     group.position.x = -5; // Переміщуємо всю групу вліво
-    const platformGeometry = new THREE.PlaneGeometry(200, 200);
+    const platformGeometry = new THREE.PlaneGeometry(2, 2);
     const platformMaterial = new THREE.MeshStandardMaterial({ color: 0xcccccc });
     const platform = new THREE.Mesh(platformGeometry, platformMaterial);
     platform.rotation.x = -Math.PI / 2; // Робимо її горизонтальною
